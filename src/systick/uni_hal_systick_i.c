a8782ea3fc990dd16c6a78ecbd2f924a1792d1e0
Uni Libraries <team@uni-libraries.net>
2024-09-10 15:29:51 +0300

    systick: fix compilation on PC

--- src/systick/uni_hal_systick_i.c
+++ src/systick/uni_hal_systick_i.c
@@ -1,28 +1,23 @@
-#if !defined(UNI_HAL_TARGET_MCU_PC)
-
 //
 // Includes
 //
 
-// uni_hal
+// Uni.HAL
 #include "systick/uni_hal_systick.h"
-
-
-//
-// Extern
-//
-
-extern void xPortSysTickHandler(void);
-extern uint32_t xTaskGetSchedulerState( void );
-#define taskSCHEDULER_NOT_STARTED    ( 1U )
-
-extern volatile uint32_t g_uni_hal_systick_counter;
 
 
 
 //
 // Handlers
 //
+
+#if !defined(UNI_HAL_TARGET_MCU_PC)
+
+extern void xPortSysTickHandler(void);
+extern uint32_t xTaskGetSchedulerState( void );
+#define taskSCHEDULER_NOT_STARTED    ( 1U )
+
+extern volatile uint32_t g_uni_hal_systick_counter;
 
 __attribute__((unused)) void SysTick_Handler(void) {
     g_uni_hal_systick_counter++;
