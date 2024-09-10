00135907e876819709ee977c46587ecaf46a5195
Uni Libraries <team@uni-libraries.net>
2024-09-11 01:06:54 +0300

    core: fix includes

--- src/core/uni_hal_core.h
+++ src/core/uni_hal_core.h
@@ -9,10 +9,8 @@
 
 // uni_hal
 #include "core/uni_hal_core_enum.h"
-#if defined(UNI_HAL_MCU_CM7)
-    #include "core/uni_hal_hal_core_cm7.h"
-#endif
 #if defined(UNI_HAL_TARGET_MCU_STM32H743)
+    #include "core/uni_hal_core_cm7.h"
     #include "core/uni_hal_core_stm32h7.h"
 #endif
 
@@ -23,5 +21,7 @@
 //
 
 void uni_hal_core_irq_init(void);
+
 bool uni_hal_core_irq_enable(uni_hal_core_irq_e irq, uint32_t priority_group, uint32_t priority_subgroup);
+
 uint32_t uni_hal_core_irq_getnum(uni_hal_core_irq_e irq);
