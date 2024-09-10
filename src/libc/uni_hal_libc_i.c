bc21cdce9b69f21333322a40037b16e145eb53cc
Uni Libraries <team@uni-libraries.net>
2024-09-10 23:19:24 +0300

    libc: fix compilation on non-PC targets

--- src/libc/uni_hal_libc_i.c
+++ src/libc/uni_hal_libc_i.c
@@ -4,66 +4,22 @@
 
 // stdlib
 #include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
-
-
-#if defined(NEWLIB)
-
-#if !defined(__clang__)
-#include <reent.h>
-#endif
-
-// uni_hal
-#include "uni_hal_common_libc.h"
-
-
-
-//
-// Implementation
-//
-
-int *__errno(void) {
-#if defined(__clang__)
-    return NULL;
-#else
-    return &_REENT->_errno;
-#endif
-}
-
-int _close(int) {
-    errno = ENOSYS;
-    return -1;
-}
-
-int _kill(int, int) {
-    errno = ENOSYS;
-    return -1;
-}
-
-off_t _lseek(int, off_t, int) {
-    errno = ENOSYS;
-    return -1;
-}
-
-int _getpid() {
-    errno = ENOSYS;
-    return -1;
-}
-
-//
-// Includes
-//
-
-// stdlib
-#include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdio.h>
 
-// uni_hal
-#include "uni_hal_common.h"
-#include "uni_hal_io.h"
-#include "uni_hal_io_stdio.h"
+// Uni.Common
+#include <uni_common.h>
+
+// Uni.HAL
+#include "io/uni_hal_io.h"
+#include "io/uni_hal_io_stdio.h"
+
+// newlib
+#if defined(_NEWLIB_VERSION) && !defined(__clang__)
+#include <reent.h>
+#endif
 
 
 
@@ -73,16 +29,63 @@ int _getpid() {
 
 extern uni_hal_stdio_context_t g_uni_hal_io_stdio_ctx;
 
-#if defined(__clang__)
+#if defined(_NEWLIB_VERSION) && defined(__clang__)
 FILE* const stdout = nullptr;
 #endif
 
 
+
 //
-// Standard I/O Functions
+// Functions
 //
 
-int _read(int UNI_HAL_COMPILER_UNUSED_VAR(file), char *ptr, int len) {
+#if !defined(UNI_HAL_TARGET_MCU_PC)
+
+int *__errno(void) {
+#if defined(__clang__)
+    return NULL;
+#else
+    return &_REENT->_errno;
+#endif
+}
+
+
+int _close(int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+
+int _kill(int, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+
+off_t _lseek(int, off_t, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+
+int _getpid() {
+    errno = ENOSYS;
+    return -1;
+}
+
+
+int _fstat(int UNI_COMMON_COMPILER_UNUSED_VAR(file), struct stat *st) {
+    if (st != nullptr) {
+        st->st_mode = S_IFCHR;
+    }
+    return 0;
+}
+
+
+int _isatty(int UNI_COMMON_COMPILER_UNUSED_VAR(file)) { return 1; }
+
+
+int _read(int UNI_COMMON_COMPILER_UNUSED_VAR(file), char *ptr, int len) {
     int result = -1;
 
     if (g_uni_hal_io_stdio_ctx.io_context && ptr) {
@@ -97,7 +100,7 @@ int _read(int UNI_HAL_COMPILER_UNUSED_VAR(file), char *ptr, int len) {
 }
 
 
-ssize_t _write(int UNI_HAL_COMPILER_UNUSED_VAR(file), const void *ptr, size_t len) {
+ssize_t _write(int UNI_COMMON_COMPILER_UNUSED_VAR(file), const void *ptr, size_t len) {
     int result = -1;
 
     if (g_uni_hal_io_stdio_ctx.io_context && ptr) {
@@ -111,17 +114,4 @@ ssize_t _write(int UNI_HAL_COMPILER_UNUSED_VAR(file), const void *ptr, size_t le
     return result;
 }
 
-
-int _fstat(int UNI_HAL_COMPILER_UNUSED_VAR(file), struct stat *st) {
-    if (st != nullptr) {
-        st->st_mode = S_IFCHR;
-    }
-    return 0;
-}
-
-
-int _isatty(int UNI_HAL_COMPILER_UNUSED_VAR(file)) { return 1; }
-
-
 #endif
-
