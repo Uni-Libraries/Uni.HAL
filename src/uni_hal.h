37b6c08588a69f7508ddcb0dd418a8b31b524518
Uni Libraries <team@uni-libraries.net>
2024-09-10 14:04:19 +0300

    header/main: fix typo

--- src/uni_hal.h
+++ src/uni_hal.h
@@ -4,22 +4,23 @@
 extern "C" {
 #endif
 
-#include "adc/uni_hal_hal_adc.h"
-#include "core/uni_hal_hal_core.h"
-#include "crc/uni_hal_hal_crc.h"
-#include "dma/uni_hal_hal_dma.h"
-#include "dwt/uni_hal_hal_dwt.h"
-#include "gpio/uni_hal_hal_gpio.h"
-#include "i2c/uni_hal_hal_i2c.h"
+#include "adc/uni_hal_adc.h"
+#include "core/uni_hal_core.h"
+#include "crc/uni_hal_crc.h"
+#include "dma/uni_hal_dma.h"
+#include "dwt/uni_hal_dwt.h"
+#include "gpio/uni_hal_gpio.h"
+#include "i2c/uni_hal_i2c.h"
 #include "io/uni_hal_io.h"
-#include "iwdg/uni_hal_hal_iwdg.h"
-#include "pwr/uni_hal_hal_pwr.h"
-#include "rcc/uni_hal_hal_rcc.h"
-#include "rng/uni_hal_hal_rng.h"
-#include "spi/uni_hal_hal_spi.h"
-#include "systick/uni_hal_hal_systick.h"
-#include "tim/uni_hal_hal_tim.h"
-#include "uart/uni_hal_hal_uart.h"
+#include "io/uni_hal_io_stdio.h"
+#include "iwdg/uni_hal_iwdg.h"
+#include "pwr/uni_hal_pwr.h"
+#include "rcc/uni_hal_rcc.h"
+#include "rng/uni_hal_rng.h"
+#include "spi/uni_hal_spi.h"
+#include "systick/uni_hal_systick.h"
+#include "tim/uni_hal_tim.h"
+#include "uart/uni_hal_uart.h"
 
 #if defined(__cplusplus)
 }
