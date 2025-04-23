aa29efa131e08e32c9f86ea7cf0d9dbc46aba988
Mikhail Paulyshka <me@mixaill.net>
2025-04-23 14:31:39 +0300

    cmake: prevent re-adding uni.common package
    Ensures that the uni.common package is only added once
    by wrapping the CPMAddPackage call in a conditional
    check for the existence of the target.

--- cmake/cpm/cpm-uni.common.cmake
+++ cmake/cpm/cpm-uni.common.cmake
@@ -1,5 +1,7 @@
-CPMAddPackage(
-        NAME uni.common
-        GITHUB_REPOSITORY Uni-Libraries/Uni.Common
-        GIT_TAG 0dc9962626bab63de76c68a0080befa2d9fb7bf8
-)
+if(NOT TARGET uni.common)
+    CPMAddPackage(
+            NAME uni.common
+            GITHUB_REPOSITORY Uni-Libraries/Uni.Common
+            GIT_TAG 0dc9962626bab63de76c68a0080befa2d9fb7bf8
+    )
+endif()
