7c13dc9d85a765f4b1530a9341b7904998ec9d7e
Mikhail Paulyshka <me@mixaill.net>
2025-04-29 00:36:08 +0300

    3rdparty/freertos_kernel: fix FreeRTOS logging

--- 3rdparty/freertos_kernel/src_hooks/freertos_printf.c
+++ 3rdparty/freertos_kernel/src_hooks/freertos_printf.c
@@ -6,18 +6,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-// segger
-#if !defined(UNI_HAL_TARGET_MCU_PC)
-    #include "SEGGER_RTT.h"
-#endif
+
+
+//
+// Functions
+//
 
 void vLoggingPrintf( const char * pcFormat, ... ) {
     va_list ParamList;
     va_start(ParamList, sFormat);
-#if defined(UNI_HAL_TARGET_MCU_PC)
     vprintf(pcFormat, ParamList);
-#else
-    SEGGER_RTT_vprintf(0, pcFormat, &ParamList);
-#endif
     va_end(ParamList);
 }
