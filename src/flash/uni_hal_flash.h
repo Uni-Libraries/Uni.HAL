5d917d5d6a89cb66250ea1242e41441fb1ff00b0
Mikhail Paulyshka <me@mixaill.net>
2025-06-28 18:22:55 +0300

    Adds flash bank erase functionality
    Implements a function to erase an entire flash bank.
    This functionality is required for certain update scenarios.
    
    Initializes the flash interface with interrupt support for asynchronous operations.
    This allows flash operations to occur in the background, improving system responsiveness.

--- src/flash/uni_hal_flash.h
+++ src/flash/uni_hal_flash.h
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void uni_hal_flash_init();
+
 size_t uni_hal_flash_get_size();
 
 size_t uni_hal_flash_read(size_t addr, size_t size, uint8_t *dst);
@@ -10,5 +12,7 @@ size_t uni_hal_flash_read(size_t addr, size_t size, uint8_t *dst);
 size_t uni_hal_flash_write(size_t addr, size_t size, uint8_t *dst);
 
 bool uni_hal_flash_erase(size_t addr, size_t size);
+
+bool uni_hal_flash_erase_bank();
 
 bool uni_hal_flash_swap_banks(void);
