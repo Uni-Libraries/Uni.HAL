f6118082bac682026a28c65e8b85e92c0614f9d9
Mikhail Paulyshka <me@mixaill.net>
2026-07-02 02:42:18 +0300

    core/stm32l4: Add support for TIM1 Update interrupt

--- src/core/uni_hal_core_stm32l4.c
+++ src/core/uni_hal_core_stm32l4.c
@@ -47,6 +47,9 @@ IRQn_Type uni_hal_core_irq_getnum(uni_hal_core_irq_e irq)
     case UNI_HAL_CORE_IRQ_TIM_1:
         result = TIM1_CC_IRQn;
         break;
+    case UNI_HAL_CORE_IRQ_TIM_1_UP:
+        result = TIM1_UP_TIM16_IRQn;
+        break;
     case UNI_HAL_CORE_IRQ_UART_1:
         result = USART1_IRQn;
         break;
